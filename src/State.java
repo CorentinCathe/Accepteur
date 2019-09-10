0f77a0d28df674348d1862b1917f560c916b6991
e189722u <co.cathelinais@gmail.com>
2019-09-10 17:21:05 +0200

    first commit

---
+++ src/State.java
@@ -0,0 +1,13 @@
+public class State {
+
+    private int statId;
+    private String statName;
+
+
+    public State(int statId, String statName) {
+        this.statId = statId;
+        this.statName = statName;
+    }
+
+
+}
